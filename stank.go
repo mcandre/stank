8f55d0bbd09295231e1cca1ead517086495044a3
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-09-12 16:32:32 -0500

    bump to v0.0.11

--- stank.go
+++ stank.go
@@ -10,7 +10,7 @@ import (
 )
 
 // Version is semver.
-const Version = "0.0.10"
+const Version = "0.0.11"
 
 // Smell describes the overall impression of a file's POSIXyness,
 // using several factors to determine with a reasonably high accuracy
